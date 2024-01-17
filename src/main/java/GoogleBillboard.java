33be18e5f3fbf8ad410be8d0ae8bdac992c0c382
stephelophagus <139821295+stephelophagus@users.noreply.github.com>
2024-01-17 09:11:43 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -9,7 +9,17 @@ public class GoogleBillboard {
 
 	//Finish this function
 	public boolean isPrime(){
+		if (num <= 1 ){
+			return false; 
+		}
+		for (int i = 2; i <= Math.sqrt (num); i++){
+			if (num%i == 0){
+				return false; 
+			}
+		}
+		return true; 
 	}
+
 
 }
 
